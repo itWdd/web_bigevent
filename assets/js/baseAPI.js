784338a5efcdb05aa3e029e5f2a9f0b082dc4644
itwdd <www.1140606988@qq.com>
2023-03-28 22:24:09 +0800

    提交了index页面

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,8 +1,29 @@
 // 每次调用 $.get、post、ajax时，都会先调用 ajaxPrefilter 函数，
 // 在这个函数中，可以拿到我们给ajax提供的配置对象
 
-$.ajaxPrefilter(function(options){
+$.ajaxPrefilter(function (options) {
     // 再发起真正的请求前，统一拼接根路径
     // console.log(options.url);
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+
+
+    // 统一为有权限的接口添加headers请求头
+    //判断url是否为/my/ 此url需要请求头
+    // 如果要检索的字符串值没有出现，则该方法返回 -1。
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 无论请求成功和失败都会执行此函数
+    options.complete = function (res) {
+        console.log(res);
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 1.清空token
+            localStorage.removeItem('token')
+            // 2.强制跳转到login
+            location.href = '/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,66 @@
+$(function () {
+    getUerInfo()
+
+    // 退出功能
+    var layer = layui.layer
+    $('.btnLogout').on('click', function () {
+        layer.confirm('确定需要退出?', { icon: 3, title: '提示' }, function (index) {
+            // 清除本地存储
+            localStorage.removeItem('token')
+            // 跳转
+            location.href = '/login.html'
+            // 关闭confirm询问框
+            layer.close(index);
+        });
+    })
+})
+
+// 获取用户的基本信息
+function getUerInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        // headers: {
+        //     // 从网页缓存中获取之前存的token，没有则空值
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function (res) {
+            // console.log(res);
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败！')
+            }
+            // 成功后调用渲染用户头像函数
+            randerAvatar(res.data)
+        }
+        // // 无论请求成功和失败都会执行此函数
+        // complete:function(res){
+        //     console.log(res);
+        //     if(res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！'){
+        //         // 1.清空token
+        //         localStorage.removeItem('token')
+        //         // 2.强制跳转到login
+        //         location.href = '/login.html'
+        //     }
+        // }
+    })
+}
+
+// 渲染用户头像
+function randerAvatar(user) {
+    // 1.data中有登录的名称和昵称，判断哪个没有就选另一个
+    var name = user.username || user.nickname
+    // 2.设置欢迎文本
+    $('.welcome').html('欢迎&nbsp&nbsp' + name)
+    // 3.按需获取用户头像
+    if (user.user_pic !== null) {
+        // 显示图片头像
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.userAvater').hide()
+    } else {
+        // 显示文字头像
+        $('.layui-nav-img').hide()
+        // 获取文字首字并设置为大写
+        var first = name[0].toUpperCase()
+        $('.userAvater').html(first).show()
+    }
+}
